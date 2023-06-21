a82334b5827b33b544f0efbaac7a7de2aabf4db7
Anatoliy Ge <ge.anatoliy@gmail.com>
2023-06-21 16:06:12 +0500

    add Service, Request and Endpoint

---
+++ RickAndMortyTestTask/APIClients/RMService.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+final class RMService {
+    
+    static let shared = RMService()
+    
+    private init() {}
+    
+    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+        
+    }
+}
