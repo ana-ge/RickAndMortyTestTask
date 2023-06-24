6a2b1fbe3cf8b08c908228dcf7bab6ea5133f9ec
Anatoliy Ge <ge.anatoliy@gmail.com>
2023-06-24 14:16:42 +0500

    add RMCharacterCollectionViewCellViewModel

---
+++ RickAndMortyTestTask/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+final class RMCharacterCollectionViewCellViewModel {
+    public let characterName: String
+    private let characterStatus: RMCharacterStatus
+    private let characterImageUrl: URL?
+    
+    init(
+        charachterName: String,
+        characterStatus: RMCharacterStatus,
+        characterImageUrl: URL?
+    ) {
+        self.characterName = charachterName
+        self.characterStatus = characterStatus
+        self.characterImageUrl = characterImageUrl
+    }
+    
+    public var characterStatusText: String {
+        return characterStatus.rawValue
+    }
+    
+    public func fetchImage(completion: @escaping (Result<Data,Error>) -> Void) {
+        guard let url = characterImageUrl else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
