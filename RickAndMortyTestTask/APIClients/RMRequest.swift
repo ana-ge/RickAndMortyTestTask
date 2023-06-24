13a57e74caca49c010d8b52711a511e83fa64a13
Anatoliy Ge <ge.anatoliy@gmail.com>
2023-06-24 11:35:55 +0500

    create new folder for API clients

--- RickAndMortyTestTask/APIClients/RMRequest.swift
+++ RickAndMortyTestTask/APIClients/RMRequest.swift
@@ -8,7 +8,7 @@ final class RMRequest {
     /// Desired Endpoint
     private let endpoint: RMEndpoint
     /// Path components for API ,if any
-    private let pathComponents: Set<String>
+    private let pathComponents: [String]
     /// Query arguments for API, if any
     private let queryParameters: [URLQueryItem]
     
@@ -45,11 +45,15 @@ final class RMRequest {
     
     public init(
         endpoint: RMEndpoint,
-        pathComponents: Set<String> = [],
+        pathComponents: [String] = [],
         queryParameters: [URLQueryItem] = []
     ) {
         self.endpoint = endpoint
         self.pathComponents = pathComponents
         self.queryParameters = queryParameters
     }
+}
+
+extension RMRequest {
+    static let listCharactersRequests = RMRequest(endpoint: .character)
 }
