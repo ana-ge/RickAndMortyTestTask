bbeef5912a97297bd0dd383ce6c6170849a473a8
Anatoliy Ge <ge.anatoliy@gmail.com>
2023-06-25 12:27:19 +0500

    add Status prefix

--- RickAndMortyTestTask/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMortyTestTask/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -16,7 +16,7 @@ final class RMCharacterCollectionViewCellViewModel {
     }
     
     public var characterStatusText: String {
-        return characterStatus.rawValue
+        return "Status: \(characterStatus.rawValue)"
     }
     
     public func fetchImage(completion: @escaping (Result<Data,Error>) -> Void) {
