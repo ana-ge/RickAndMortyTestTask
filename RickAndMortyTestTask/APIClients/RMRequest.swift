ad4d2a649ba68734d619b0008811cc7734dcbce3
Anatoliy Ge <ge.anatoliy@gmail.com>
2023-06-28 14:21:50 +0500

    refactor request url

--- RickAndMortyTestTask/APIClients/RMRequest.swift
+++ RickAndMortyTestTask/APIClients/RMRequest.swift
@@ -53,6 +53,7 @@ final class RMRequest {
         self.queryParameters = queryParameters
     }
     
+    /// Attempt to create request
     convenience init?(url: URL) {
         let string = url.absoluteString
         if !string.contains(Constants.baseUrl) {
@@ -63,8 +64,13 @@ final class RMRequest {
             let components = trimmed.components(separatedBy: "/")
             if !components.isEmpty {
                 let endpointString = components[0]
+                var pathComponents: [String] = []
+                if components.count > 1 {
+                    pathComponents = components
+                    pathComponents.removeFirst()
+                }
                 if let rmEndpoint = RMEndpoint(rawValue: endpointString) {
-                    self.init(endpoint: rmEndpoint)
+                    self.init(endpoint: rmEndpoint, pathComponents: pathComponents)
                     return
                 }
             }
