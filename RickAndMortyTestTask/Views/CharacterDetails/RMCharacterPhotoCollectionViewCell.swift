368d7defbe3abdf5d7fbc22fc0dcd806731ef224
Anatoliy Ge <ge.anatoliy@gmail.com>
2023-06-29 13:08:37 +0500

    add imageView and constraints

--- RickAndMortyTestTask/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
+++ RickAndMortyTestTask/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
@@ -3,8 +3,18 @@ import UIKit
 final class RMCharacterPhotoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterPhotoCollectionViewCell"
     
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.addSubview(imageView)
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -12,14 +22,29 @@ final class RMCharacterPhotoCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupLayout() {
-        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        imageView.image = nil
     }
     
     public func configure(with viewModel: RMCharacterPhotoCollectionViewCellViewModel) {
-        
+        viewModel.fetchImage { [weak self] result in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async {
+                    self?.imageView.image = UIImage(data: data)
+                }
+            case .failure:
+                break
+            }
+        }
     }
 }
