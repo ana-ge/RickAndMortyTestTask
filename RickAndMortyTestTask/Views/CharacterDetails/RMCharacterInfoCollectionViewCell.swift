456c5feb74fdeca52437d28738d9d164b9a4a522
Anatoliy Ge <ge.anatoliy@gmail.com>
2023-06-29 12:26:38 +0500

    add cells

---
+++ RickAndMortyTestTask/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
+    static let cellIdentifier = "RMCharacterInfoCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    public func configure(with viewModel: RMCharacterInfoCollectionViewCellViewModel) {
+        
+    }
+}
---
+++ RickAndMortyTestTask/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+final class RMCharacterPhotoCollectionViewCell: UICollectionViewCell {
+    static let cellIdentifier = "RMCharacterPhotoCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    public func configure(with viewModel: RMCharacterPhotoCollectionViewCellViewModel) {
+        
+    }
+}
